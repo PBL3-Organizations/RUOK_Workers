820aeffdae09e40562e4fa975a66e6603492d6dc
coderrim <hyorim2102@gmail.com>
2024-05-12 21:41:15 +0900

    dashboard

--- app/src/main/java/com/example/ruok_workers/DashboardFragment.kt
+++ app/src/main/java/com/example/ruok_workers/DashboardFragment.kt
@@ -6,51 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.ruok_workers.databinding.FragmentDashboardBinding
 
 
 class DashboardFragment : Fragment() {
+    lateinit var binding: FragmentDashboardBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
-        val btnList = view.findViewById<Button>(R.id.btnList)
-        val btnLocation = view.findViewById<Button>(R.id.btnLocation)
-
-        //문진표 목록
-        btnList.setOnClickListener {
-            navigateToFragment(ListFragment())
-        }
-
-        //지도에서 노숙인 위치 정보 보기
-        btnLocation.setOnClickListener {
-            navigateToFragment(LocationTrackingFragment())
-        }
-
-        return view
+        return binding!!.root
     }
 
-    fun navigateToFragment(fragment: Fragment) {
-        val fragmentManager = requireActivity().supportFragmentManager
-        val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_dashboard, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
 
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -59,8 +36,7 @@ class DashboardFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             DashboardFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
