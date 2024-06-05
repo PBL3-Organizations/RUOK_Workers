bacfabb64900f52e05d95b932fa537718e75c9a5
alimhanhan <alim11@naver.com>
2024-06-05 11:17:45 +0900

    add dummy data for during

--- app/src/main/java/com/example/ruok_workers/BriefingDuringFragment.kt
+++ app/src/main/java/com/example/ruok_workers/BriefingDuringFragment.kt
@@ -2,14 +2,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
-import com.example.ruok_workers.BriefingAddFragment
 import com.example.ruok_workers.R
 
 class BriefingDuringFragment : Fragment() {
 
-    private lateinit var addPostButton: Button
+    private val posts = ArrayList<String>()
+    private lateinit var listView: ListView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,28 +19,38 @@ class BriefingDuringFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_briefing_during, container, false)
 
-        // '새 글 작성' 버튼 찾기
-        addPostButton = view.findViewById<Button>(R.id.btn_add_post2)
+        // 게시글 목록을 나타내는 ListView 찾기
+        listView = view.findViewById<ListView>(R.id.list_view_posts)
+
+        // 더미 데이터 생성
+        posts.addAll(listOf(
+            "게시글 1",
+            "게시글 2",
+            "게시글 3"
+            // 더 많은 게시글을 추가
+        ))
+
+        // ArrayAdapter를 사용하여 ListView에 데이터 설정
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, posts)
+        listView.adapter = adapter
+
 
         return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // '새 글 작성' 버튼을 항상 보여주기(디폴트값)
-        addPostButton.visibility = View.VISIBLE
-
-        // '새 글 작성' 버튼에 클릭 리스너 설정
-        addPostButton.setOnClickListener {
-            // '새 글 작성' 버튼을 클릭 시 BriefingAddFragment로 이동
-            val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.fragment_container, BriefingAddFragment())
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.commit()
-
-            // 새 Fragment로 이동할 때 '새 글 작성' 버튼 숨기기
-            addPostButton.visibility = View.GONE
-        }
+    // 새 게시글을 추가하는 메서드
+    fun addPost(newPost: String) {
+        posts.add(newPost)
+        // Adapter에 데이터 변경을 알림
+        (listView.adapter as ArrayAdapter<*>).notifyDataSetChanged()
     }
+
+    fun addPostBelowThird(newPost: String) {
+        // 가장 최신 게시물 아래에 새 게시물을 추가
+        posts.add(posts.size - 1, newPost)
+        // 데이터 변경을 어댑터에 알림
+        (listView.adapter as ArrayAdapter<*>).notifyDataSetChanged()
+    }
+
+
 }
