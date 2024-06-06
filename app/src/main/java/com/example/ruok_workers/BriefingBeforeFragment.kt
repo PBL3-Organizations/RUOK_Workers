26c419d40dc0d958b912ed03c4be14bde44bf81a
alimhanhan <alim11@naver.com>
2024-06-06 13:07:12 +0900

    add briefing board list -> detail

--- app/src/main/java/com/example/ruok_workers/BriefingBeforeFragment.kt
+++ app/src/main/java/com/example/ruok_workers/BriefingBeforeFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.AdapterView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class BriefingBeforeFragment : Fragment() {
 
@@ -12,7 +17,38 @@ class BriefingBeforeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_briefing_before, container, false)
+        // Fragment의 레이아웃을 인플레이트
+        val view = inflater.inflate(R.layout.fragment_briefing_before, container, false)
+
+        // 더미 데이터
+        val dummyData = arrayOf("Before 1", "아이템 2", "아이템 3", "아이템 4", "아이템 5")
+
+        // ListView를 찾기
+        val listView: ListView = view.findViewById(R.id.listView_briefing_before)
+
+        // ArrayAdapter를 생성하여 ListView를 더미 데이터로 채우기
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, dummyData)
+
+        // ListView에 어댑터를 설정
+        listView.adapter = adapter
+
+        // ListView의 항목을 클릭할 때 실행할 동작을 정의
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            // 클릭된 항목의 위치(position)에 따라 Fragment 전환 코드를 작성
+            when (position) {
+                0 -> {
+                    // BriefingDetailFragment와 연결
+                    val fragment = BriefingDetailFragment()
+                    val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+                    val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.replace(R.id.rootLayout, fragment)
+                    fragmentTransaction.addToBackStack(null)
+                    fragmentTransaction.commit()
+                }
+                // 다른 항목을 클릭했을 때 필요한 작업을 추가
+            }
+        }
+
+        return view
     }
 }
