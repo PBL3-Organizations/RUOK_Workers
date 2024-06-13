f6c252664d61ee85ab8cceddb2acc7114ad4534c
Hanbyulha <bd7845@swu.ac.kr>
2024-06-13 16:16:55 +0900

    상담내역 작성&수정 건강상태 버튼 클릭시 색 변경

--- app/src/main/java/com/example/ruok_workers/QuestionnaireFragment.kt
+++ app/src/main/java/com/example/ruok_workers/QuestionnaireFragment.kt
@@ -3,10 +3,12 @@ package com.example.ruok_workers
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.os.PowerManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -32,6 +34,29 @@ class QuestionnaireFragment : Fragment() {
                 val DashboardActivity = activity as DashboardActivity
                 DashboardActivity.setFragment(PhotoAddFragment())
             }
+        //건강상태 버튼 클릭시 색변경
+        binding.btnQuestionGood.setOnClickListener {
+            setFirstBtnColor()
+            binding.btnQuestionGood.setBackgroundColor(Color.GREEN)
+        }
+        binding.btnQuestionNotbad.setOnClickListener {
+            setFirstBtnColor()
+            binding.btnQuestionNotbad.setBackgroundColor(Color.BLUE)
+        }
+        binding.btnQuestionBad.setOnClickListener {
+            setFirstBtnColor()
+            binding.btnQuestionBad.setBackgroundColor(Color.parseColor("#FAA900"))
+        }
+        binding.btnQuestionNeed.setOnClickListener {
+            setFirstBtnColor()
+            binding.btnQuestionNeed.setBackgroundColor(Color.RED)
+        }
         return this.binding.root
+    }
+    private fun setFirstBtnColor(){
+        binding.btnQuestionGood.setBackgroundColor(Color.parseColor("#8F9090"))
+        binding.btnQuestionNotbad.setBackgroundColor(Color.parseColor("#8F9090"))
+        binding.btnQuestionBad.setBackgroundColor(Color.parseColor("#8F9090"))
+        binding.btnQuestionNeed.setBackgroundColor(Color.parseColor("#8F9090"))
     }
 }
