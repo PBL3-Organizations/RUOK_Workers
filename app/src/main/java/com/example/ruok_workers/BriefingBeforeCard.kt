7d1c78e5c40b6a34d577e8efcbcd25081a9163c3
Hanbyulha <bd7845@swu.ac.kr>
2024-06-25 16:39:02 +0900

    BriefingDetailFragment 어뎁터,아이템 리스트 추가

---
+++ app/src/main/java/com/example/ruok_workers/BriefingBeforeCard.kt
@@ -0,0 +1,9 @@
+package com.example.ruok_workers
+
+class BriefingBeforeCard (beforeTitle:String, beforeTime:String) {
+
+    var beforeTitle:String = beforeTitle
+        private set
+    var beforeTime:String = beforeTime
+        private set
+}
