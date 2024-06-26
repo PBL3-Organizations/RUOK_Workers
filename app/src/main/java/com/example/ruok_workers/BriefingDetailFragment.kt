05eec60432a5a8d2b631474f77ca59959c4ec9a5
alimhanhan <alim11@naver.com>
2024-06-26 22:06:52 +0900

    error correction -> buttonback rendering

--- app/src/main/java/com/example/ruok_workers/BriefingDetailFragment.kt
+++ app/src/main/java/com/example/ruok_workers/BriefingDetailFragment.kt
@@ -1,22 +1,21 @@
 package com.example.ruok_workers
 
+import BriefingBoardFragment
 import android.app.AlertDialog
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.ruok_workers.databinding.FragmentBriefingDetailBinding
 
 class BriefingDetailFragment : Fragment() {
-    lateinit var binding:FragmentBriefingDetailBinding
+    lateinit var binding: FragmentBriefingDetailBinding
     lateinit var dbManager: DBManager
     lateinit var sqlitedb: SQLiteDatabase
     var b_num = -1
@@ -24,7 +23,7 @@ class BriefingDetailFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
+            b_num = it.getInt("b_num", -1)
         }
     }
 
@@ -42,57 +41,70 @@ class BriefingDetailFragment : Fragment() {
         val tvDetailTimestamp = view.findViewById<TextView>(R.id.tvDetailTimestamp)
         val tvBriefingDetails = view.findViewById<TextView>(R.id.etBriefingDetails)
 
-
-        //데이터베이스 연동
+        // 데이터베이스 연동
         dbManager = DBManager(requireContext(), "RUOKsample", null, 1)
         sqlitedb = dbManager.readableDatabase
 
-        b_num =arguments?.getInt("b_num",0)!!
-        var cursor:Cursor
-        val sql = "SELECT b.b_num, b.b_title, b.b_content, m.m_name, b.b_time FROM briefing b JOIN member m ON m.m_num = b.m_num WHERE b.b_num = ?;"
-        cursor = sqlitedb.rawQuery(sql, arrayOf(b_num.toString()))
-        cursor.moveToNext()
-            tvDetailAuthor.text = cursor.getString(cursor.getColumnIndexOrThrow("m.m_name"))
-            tvDetailTitle.text = cursor.getString(cursor.getColumnIndexOrThrow("b.b_title"))
-            tvDetailTimestamp.text = cursor.getString(cursor.getColumnIndexOrThrow("b.b_time"))
-            tvBriefingDetails.text = cursor.getString(cursor.getColumnIndexOrThrow("b.b_content"))
+        // b_num을 확인하고, 유효하지 않은 경우 예외 처리
+        if (b_num == -1) {
+            Log.e("BriefingDetailFragment", "Invalid b_num")
+            return view
+        }
+
+        val sql = """
+            SELECT b.b_num, b.b_title AS title, b.b_content AS content, m.m_name AS author, b.b_time AS timestamp 
+            FROM briefing b 
+            JOIN member m ON m.m_num = b.m_num 
+            WHERE b.b_num = ?
+        """
+        val cursor: Cursor = sqlitedb.rawQuery(sql, arrayOf(b_num.toString()))
+
+        if (cursor.moveToFirst()) {
+            tvDetailAuthor.text = cursor.getString(cursor.getColumnIndexOrThrow("author"))
+            tvDetailTitle.text = cursor.getString(cursor.getColumnIndexOrThrow("title"))
+            tvDetailTimestamp.text = cursor.getString(cursor.getColumnIndexOrThrow("timestamp"))
+            tvBriefingDetails.text = cursor.getString(cursor.getColumnIndexOrThrow("content"))
+        } else {
+            Log.e("BriefingDetailFragment", "No data returned from query")
+        }
         cursor.close()
         sqlitedb.close()
         dbManager.close()
-        var b_title:String = tvDetailTitle.text.toString()
-        var b_content:String = tvBriefingDetails.text.toString()
 
         buttonEdit.setOnClickListener {
             val bundle = Bundle()
             bundle.putInt("b_num", b_num)
-            bundle.putString("b_title",b_title)
-            bundle.putString("b_content",b_content)
+            bundle.putString("b_title", tvDetailTitle.text.toString())
+            bundle.putString("b_content", tvBriefingDetails.text.toString())
 
-            val BriefingRevisionFragment = BriefingRevisionFragment()
-            BriefingRevisionFragment.arguments = bundle
-            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.rootLayout, BriefingRevisionFragment).commit()
-//            val DashboardActivity = activity as DashboardActivity
-//            DashboardActivity.setFragment(BriefingRevisionFragment())
+            val briefingRevisionFragment = BriefingRevisionFragment()
+            briefingRevisionFragment.arguments = bundle
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.rootLayout, briefingRevisionFragment)
+                .commit()
         }
 
-
         buttonBack.setOnClickListener {
-            requireActivity().onBackPressed()
+            val briefingBoardFragment = BriefingBoardFragment()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.rootLayout, briefingBoardFragment)
+                .commit()
         }
 
         buttonDelete.setOnClickListener {
             val alertDialogBuilder = AlertDialog.Builder(requireContext())
             alertDialogBuilder.setTitle("삭제하시겠습니까?")
-            alertDialogBuilder.setPositiveButton("예") { dialog, which ->
+            alertDialogBuilder.setPositiveButton("예") { _, _ ->
                 // 삭제 동작을 여기에 추가
-                //데이터베이스 연동
                 dbManager = DBManager(requireContext(), "RUOKsample", null, 1)
+                sqlitedb = dbManager.writableDatabase
+                sqlitedb.delete("briefing", "b_num = ?", arrayOf(b_num.toString()))
+                sqlitedb.close()
                 dbManager.close()
-
                 requireActivity().onBackPressed()
             }
-            alertDialogBuilder.setNegativeButton("아니오") { dialog, which ->
-                // 아무 동작 없음
+            alertDialogBuilder.setNegativeButton("아니오") { dialog, _ ->
+                dialog.dismiss()
             }
             alertDialogBuilder.show()
         }
