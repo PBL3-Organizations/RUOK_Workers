94088779245df9b3366ebf1710b48304ab89ca05
coderrim <hyorim2102@gmail.com>
2024-11-13 10:56:14 +0900

    카운팅 코스 하나만 & 뒤로가기 루프 해결

--- app/src/main/java/com/example/ruok_workers/CountingAddFragment.kt
+++ app/src/main/java/com/example/ruok_workers/CountingAddFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.ruok_workers.databinding.FragmentCountingAddBinding
 import com.example.ruok_workers.databinding.FragmentDashboardBinding
 import com.example.ruok_workers.databinding.FragmentLocationAddBinding
@@ -46,50 +47,132 @@ class CountingAddFragment : Fragment() {
         binding.radioButton3.setOnClickListener { onRadioButtonClicked(it) }
         binding.radioButton4.setOnClickListener { onRadioButtonClicked(it) }
 
-        // btnCompleteCountingAdd 클릭시 CountingAddFragment에서 CountingTableFragment로 이동
         binding.btnCountingAdd.setOnClickListener {
-            val selectedCourse = when {
-                binding.radioButton1.isChecked -> binding.radioButton1.text.toString()
-                binding.radioButton2.isChecked -> binding.radioButton2.text.toString()
-                binding.radioButton3.isChecked -> binding.radioButton3.text.toString()
-                binding.radioButton4.isChecked -> binding.radioButton4.text.toString()
-                else -> ""
+            val selectedLocations = mutableListOf<String>()
+            val selectedLocationIds = mutableListOf<Int>()
+
+            // 구역이 선택된 경우 ID와 이름을 각각 리스트에 추가
+            if (binding.checkBox11.isChecked) {
+                selectedLocations.add(binding.checkBox11.text.toString())
+                selectedLocationIds.add(11)
+            }
+            if (binding.checkBox12.isChecked) {
+                selectedLocations.add(binding.checkBox12.text.toString())
+                selectedLocationIds.add(12)
+            }
+            if (binding.checkBox13.isChecked) {
+                selectedLocations.add(binding.checkBox13.text.toString())
+                selectedLocationIds.add(13)
+            }
+            if (binding.checkBox14.isChecked) {
+                selectedLocations.add(binding.checkBox14.text.toString())
+                selectedLocationIds.add(14)
+            }
+            if (binding.checkBox15.isChecked) {
+                selectedLocations.add(binding.checkBox15.text.toString())
+                selectedLocationIds.add(15)
+            }
+            if (binding.checkBox21.isChecked) {
+                selectedLocations.add(binding.checkBox21.text.toString())
+                selectedLocationIds.add(21)
+            }
+            if (binding.checkBox22.isChecked) {
+                selectedLocations.add(binding.checkBox22.text.toString())
+                selectedLocationIds.add(22)
+            }
+            if (binding.checkBox23.isChecked) {
+                selectedLocations.add(binding.checkBox23.text.toString())
+                selectedLocationIds.add(23)
+            }
+            if (binding.checkBox24.isChecked) {
+                selectedLocations.add(binding.checkBox24.text.toString())
+                selectedLocationIds.add(24)
+            }
+            if (binding.checkBox25.isChecked) {
+                selectedLocations.add(binding.checkBox25.text.toString())
+                selectedLocationIds.add(25)
+            }
+            if (binding.checkBox26.isChecked) {
+                selectedLocations.add(binding.checkBox26.text.toString())
+                selectedLocationIds.add(26)
+            }
+            if (binding.checkBox31.isChecked) {
+                selectedLocations.add(binding.checkBox31.text.toString())
+                selectedLocationIds.add(31)
+            }
+            if (binding.checkBox32.isChecked) {
+                selectedLocations.add(binding.checkBox32.text.toString())
+                selectedLocationIds.add(32)
+            }
+            if (binding.checkBox33.isChecked) {
+                selectedLocations.add(binding.checkBox33.text.toString())
+                selectedLocationIds.add(33)
+            }
+            if (binding.checkBox34.isChecked) {
+                selectedLocations.add(binding.checkBox34.text.toString())
+                selectedLocationIds.add(34)
+            }
+            if (binding.checkBox35.isChecked) {
+                selectedLocations.add(binding.checkBox35.text.toString())
+                selectedLocationIds.add(35)
+            }
+            if (binding.checkBox36.isChecked) {
+                selectedLocations.add(binding.checkBox36.text.toString())
+                selectedLocationIds.add(36)
+            }
+            if (binding.checkBox37.isChecked) {
+                selectedLocations.add(binding.checkBox37.text.toString())
+                selectedLocationIds.add(37)
+            }
+            if (binding.checkBox38.isChecked) {
+                selectedLocations.add(binding.checkBox38.text.toString())
+                selectedLocationIds.add(38)
+            }
+            if (binding.checkBox41.isChecked) {
+                selectedLocations.add(binding.checkBox41.text.toString())
+                selectedLocationIds.add(41)
+            }
+            if (binding.checkBox42.isChecked) {
+                selectedLocations.add(binding.checkBox42.text.toString())
+                selectedLocationIds.add(42)
+            }
+            if (binding.checkBox43.isChecked) {
+                selectedLocations.add(binding.checkBox43.text.toString())
+                selectedLocationIds.add(43)
+            }
+            if (binding.checkBox44.isChecked) {
+                selectedLocations.add(binding.checkBox44.text.toString())
+                selectedLocationIds.add(44)
+            }
+            if (binding.checkBox45.isChecked) {
+                selectedLocations.add(binding.checkBox45.text.toString())
+                selectedLocationIds.add(45)
             }
 
-            val selectedLocations = mutableListOf<String>()
-            if (binding.checkBox11.isChecked) selectedLocations.add(binding.checkBox11.text.toString())
-            if (binding.checkBox12.isChecked) selectedLocations.add(binding.checkBox12.text.toString())
-            if (binding.checkBox13.isChecked) selectedLocations.add(binding.checkBox13.text.toString())
-            if (binding.checkBox14.isChecked) selectedLocations.add(binding.checkBox14.text.toString())
-            if (binding.checkBox15.isChecked) selectedLocations.add(binding.checkBox15.text.toString())
-            if (binding.checkBox21.isChecked) selectedLocations.add(binding.checkBox21.text.toString())
-            if (binding.checkBox22.isChecked) selectedLocations.add(binding.checkBox22.text.toString())
-            if (binding.checkBox23.isChecked) selectedLocations.add(binding.checkBox23.text.toString())
-            if (binding.checkBox24.isChecked) selectedLocations.add(binding.checkBox24.text.toString())
-            if (binding.checkBox25.isChecked) selectedLocations.add(binding.checkBox25.text.toString())
-            if (binding.checkBox26.isChecked) selectedLocations.add(binding.checkBox26.text.toString())
-            if (binding.checkBox31.isChecked) selectedLocations.add(binding.checkBox31.text.toString())
-            if (binding.checkBox32.isChecked) selectedLocations.add(binding.checkBox32.text.toString())
-            if (binding.checkBox33.isChecked) selectedLocations.add(binding.checkBox33.text.toString())
-            if (binding.checkBox34.isChecked) selectedLocations.add(binding.checkBox34.text.toString())
-            if (binding.checkBox35.isChecked) selectedLocations.add(binding.checkBox35.text.toString())
-            if (binding.checkBox36.isChecked) selectedLocations.add(binding.checkBox36.text.toString())
-            if (binding.checkBox37.isChecked) selectedLocations.add(binding.checkBox37.text.toString())
-            if (binding.checkBox38.isChecked) selectedLocations.add(binding.checkBox38.text.toString())
-            if (binding.checkBox41.isChecked) selectedLocations.add(binding.checkBox41.text.toString())
-            if (binding.checkBox42.isChecked) selectedLocations.add(binding.checkBox42.text.toString())
-            if (binding.checkBox43.isChecked) selectedLocations.add(binding.checkBox43.text.toString())
-            if (binding.checkBox44.isChecked) selectedLocations.add(binding.checkBox44.text.toString())
-            if (binding.checkBox45.isChecked) selectedLocations.add(binding.checkBox45.text.toString())
+            // 선택된 구역 ID의 십의 자리를 확인해 코스 일관성 검증
+            val courseSet = selectedLocationIds.map { it / 10 }.toSet()
 
-            val bundle = Bundle()
-            bundle.putString("course", selectedCourse)
-            bundle.putStringArrayList("locations", ArrayList(selectedLocations))
-            bundle.putInt("m_num", loginNum)
+            if (courseSet.size > 1) {
+                Toast.makeText(context, "코스를 하나만 선택해주십시오.", Toast.LENGTH_SHORT).show()
+            } else {
+                val selectedCourse = when {
+                    binding.radioButton1.isChecked -> binding.radioButton1.text.toString()
+                    binding.radioButton2.isChecked -> binding.radioButton2.text.toString()
+                    binding.radioButton3.isChecked -> binding.radioButton3.text.toString()
+                    binding.radioButton4.isChecked -> binding.radioButton4.text.toString()
+                    else -> ""
+                }
 
-            val countingTableFragment = CountingTableFragment()
-            countingTableFragment.arguments = bundle
-            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.rootLayout, countingTableFragment).commit()
+                val bundle = Bundle()
+                bundle.putString("course", selectedCourse)
+                bundle.putStringArrayList("locations", ArrayList(selectedLocations))
+                bundle.putInt("m_num", loginNum)
+
+                val countingTableFragment = CountingTableFragment()
+                countingTableFragment.arguments = bundle
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(R.id.rootLayout, countingTableFragment).commit()
+            }
         }
 
         return binding.root
